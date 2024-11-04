4ce2203e77bc92ac6eeb970750a4a9d3929a7fce
juchangyang <cksdid3357@naver.com>
2024-11-04 20:53:05 +0900

    홈 페이지 수정
    로그인 안 해도 게시물 뜨게 수정, 영상도 불러오게 수정(아직 영상이 뜨진 않음)

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,20 +4,6 @@ import { useEffect, useState } from 'react';
 
 export default function HomePage() {
   const [videoList, setVideoList] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 관리하는 상태 추가
-
-  useEffect(() => {
-    const checkSession = async () => {
-      const response = await fetch('http://localhost:4000/session', {
-        method: 'GET',
-        credentials: 'include',
-      });
-      const data = await response.json();
-      setIsLoggedIn(data.loggedIn); // 로그인 상태 업데이트
-    };
-
-    checkSession();
-  }, []);
 
   useEffect(() => {
     const fetchVideos = async () => {
@@ -49,27 +35,27 @@ export default function HomePage() {
   return (
     <div className="home-container">
       <div className="video-list">
-        {isLoggedIn ? ( // 로그인 상태에 따라 비디오 리스트 표시
-          videoList === null ? (
-            <p>게시글을 불러오는 중입니다...</p>
-          ) : videoList.length > 0 ? (
-            <ul>
-              {videoList.map((video) => (
-                <li key={video._id}>
-                  <a href={`/videos/${video._id}`}>
-                    <strong>제목:</strong> {video.title} <br />
-                    <strong>설명:</strong> {video.description} <br />
-                    <strong>태그:</strong> {video.hashtags} <br />
-                    <strong>업로드 시간:</strong> {formatUploadTime(video.createdAt)} <br />
-                  </a>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>아직 게시된 비디오가 없습니다.</p>
-          )
-        ) : null} {/* 로그인하지 않은 경우에는 아무것도 렌더링하지 않음 */}
+        {videoList === null ? (
+          <p>게시글을 불러오는 중입니다...</p>
+        ) : videoList.length > 0 ? (
+          <ul className="video-grid">
+            {videoList.map((video) => (
+              <li key={video._id} className="video-item">
+                <a href={`/videos/${video._id}`}>
+                  <img
+                    src={video.thumbnailUrl}
+                    alt={video.title}
+                    className="video-thumbnail"
+                  />
+                  <div className="video-title">제목 : {video.title}</div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>아직 게시된 비디오가 없습니다.</p>
+        )}
       </div>
     </div>
-  );
+  );  
 }
